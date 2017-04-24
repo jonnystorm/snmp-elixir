c12ba1ee10025387d502b4e16d66e7541a28a7b1
Jonathan Storm <jds@idio.link>
2017-04-23 19:49:34 -0500

    Make MIB compilation tests pass
    * copy broken and obsolete mibs to `test/fixtures/broken_mibs`
    * patch broken MIBs in `test/fixtures/mibs`
    * add more logging
    * add test for broken MIBs
    * set `use ExUnit.Case, async: false`; `true` would break tests
    * minor clean-up, reformatting

--- test/snmp/mib_test.exs
+++ test/snmp/mib_test.exs
@@ -1,27 +1,36 @@
 defmodule SNMP.MIB.Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest SNMP.MIB
 
   import SNMP.MIB
 
   @moduletag :integrated
 
-  @tmp_dir "test/mib_tmp"
+  @tmp_dir      "test/mib_tmp"
+  @fixtures_dir "test/fixtures"
 
-  setup_all do
-    File.mkdir! @tmp_dir
-    File.cp_r! "test/fixtures/mibs", @tmp_dir
-
+  setup do
     on_exit fn ->
       File.rm_rf! @tmp_dir
     end
   end
 
-  test "Compiles directory of SNMP MIBs (with extension .mib) without errors" do
+  test "Compiles all good and patched SNMP MIBs in directory without errors" do
+    File.cp_r! "#{@fixtures_dir}/mibs", @tmp_dir
+
     results = compile_all @tmp_dir
+
     mib_files = Enum.map(results, fn {f, _} -> :binary.bin_to_list(f) end)
 
-    assert Enum.all?(results, fn {_, {a, _}} -> a == :ok end) == true
+    assert Enum.all?(results, fn {_, {a, _}} -> a == :ok end)
     assert :snmpc.is_consistent(mib_files) == :ok
+  end
+
+  test "Fails to compile broken SNMP MIBs" do
+    File.cp_r! "#{@fixtures_dir}/broken_mibs", @tmp_dir
+
+    results = compile_all @tmp_dir
+
+    assert Enum.all?(results, fn {_, {a, _}} -> a != :ok end)
   end
 end
