a76b66e0b2aa98fc8c0f51a030bf8736b7b715c5
Alex Lawrence <alex.lawrene@ruckuswireless.com>
2017-06-07 18:26:44 +0800

    #6 - Simplied the start-up process for the agent using snmpa_supervisor.

--- lib/snmp/discover_agent.ex
+++ lib/snmp/discover_agent.ex
@@ -14,43 +14,8 @@ defmodule SNMP.DiscoverAgent do
   end
 
   def init(opts) do
-    args = [  agent_type: :master,
-              discovery: [originating: [enable: true], terminating: [enable: true]],
-              db_dir: 'snmp_db/agent',
-              db_init_error: :create_db_and_dir,
-              config: [ dir: 'snmp/agent' ],
-           ]
-    Application.put_env(:snmp, :agent, args)
     GenServer.cast(self(), {:seed_and_start_agent, opts})
     {:ok, []}
-  end
-
-  def find_engine_id(ip_address, opts \\ []) do
-    GenServer.call(@name, {:discover_engine_id, ip_address, opts})
-  end
-
-  def handle_call({:discover_engine_id, ip_address, opts_input}, _from, state) when is_binary(ip_address) do
-    ip_address_tuple = ip_address |> :binary.bin_to_list |> List.to_tuple
-    handle_call({:discover_engine_id, ip_address_tuple, opts_input}, _from, state)
-  end
-
-  def handle_call({:discover_engine_id, ip_address, opts_input}, _from, state) do
-    clean_opts = Enum.reject(opts_input, fn({_k, v}) -> is_nil(v) end)
-    opts = Keyword.merge([port: 161, transport: :transportDomainUdpIpv4, timeout: 2000, notification: :coldStart], clean_opts)
-
-    agent_name = "discovery_agent_#{Enum.join(Tuple.to_list(ip_address), "_")}" |> to_charlist
-    {:ok, _} = :snmp_target_mib.add_addr(agent_name, opts[:transport],
-                                        {ip_address, opts[:port]}, opts[:timeout], 0, '', 'discovery_params', '', [], 2048)
-
-    {:ok, engine_id} = :snmpa.discovery(agent_name, opts[:notification])
-    {:reply, engine_id, state}
-  end
-
-  def handle_cast({:seed_and_start_agent, opts}, state) do
-    is_seeded = Enum.all?(["snmp/agent/agent.conf", "snmp/agent/standard.conf"], &(File.exists?(&1)))
-    unless is_seeded, do: seed_config(opts)
-    start_agent()
-    {:noreply, state}
   end
 
   def seed_config(opts) do
@@ -87,9 +52,13 @@ defmodule SNMP.DiscoverAgent do
 
   def start_agent() do
     Logger.info("Starting snmp agent...")
-    :snmp.start()
-    :snmpa.verbosity(:all, :info)
-    :snmp.start_agent()
+    args = [  agent_type: :master,
+              discovery: [originating: [enable: true], terminating: [enable: true]],
+              db_dir: 'snmp_db/agent',
+              db_init_error: :create_db_and_dir,
+              config: [ dir: 'snmp/agent' ],
+           ]
+    :snmpa_supervisor.start_master_sup(args)
     configure_discovery_config()
   end
 
@@ -98,6 +67,34 @@ defmodule SNMP.DiscoverAgent do
     {:ok, _} = :snmp_view_based_acm_mib.add_sec2group(:usm, '', 'discovery_group')
     {:ok, _} = :snmp_view_based_acm_mib.add_view_tree_fam('discovery', [1,3,6,1], :included, :null)
     {:ok, _} = :snmp_target_mib.add_params('discovery_params', :v3, :usm, '', :noAuthNoPriv)
+  end
+
+  def find_engine_id(ip_address, opts \\ []) do
+    GenServer.call(@name, {:discover_engine_id, ip_address, opts})
+  end
+
+  def handle_call({:discover_engine_id, ip_address, opts_input}, _from, state) when is_binary(ip_address) do
+    ip_address_tuple = ip_address |> :binary.bin_to_list |> List.to_tuple
+    handle_call({:discover_engine_id, ip_address_tuple, opts_input}, _from, state)
+  end
+
+  def handle_call({:discover_engine_id, ip_address, opts_input}, _from, state) do
+    clean_opts = Enum.reject(opts_input, fn({_k, v}) -> is_nil(v) end)
+    opts = Keyword.merge([port: 161, transport: :transportDomainUdpIpv4, timeout: 2000, notification: :coldStart], clean_opts)
+
+    agent_name = "discovery_agent_#{Enum.join(Tuple.to_list(ip_address), "_")}" |> to_charlist
+    {:ok, _} = :snmp_target_mib.add_addr(agent_name, opts[:transport],
+                                        {ip_address, opts[:port]}, opts[:timeout], 0, '', 'discovery_params', '', [], 2048)
+
+    {:ok, engine_id} = :snmpa.discovery(agent_name, opts[:notification])
+    {:reply, engine_id, state}
+  end
+
+  def handle_cast({:seed_and_start_agent, opts}, state) do
+    is_seeded = Enum.all?(["snmp/agent/agent.conf", "snmp/agent/standard.conf"], &(File.exists?(&1)))
+    unless is_seeded, do: seed_config(opts)
+    start_agent()
+    {:noreply, state}
   end
 
 end
