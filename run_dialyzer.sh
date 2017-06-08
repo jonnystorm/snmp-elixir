d23bab7158ead975980ab592a936009ba19ffdf5
Alex Lawrence <alex.lawrene@ruckuswireless.com>
2017-06-08 15:58:59 +0800

    #6 To run the dialyzer with the right set of PLT files.

---
+++ run_dialyzer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# ELIXIR_VERSION=1.4.2 OTP_RELEASE=19.2 ./run_dialyzer.sh
+export MIX_ENV=test
+
+export PLT_FILENAME=elixir-${ELIXIR_VERSION}_${OTP_RELEASE}.plt
+export PLT_TESTNAME=dialyxir_erlang-${OTP_RELEASE}_elixir-${ELIXIR_VERSION}_deps-${MIX_ENV}.plt
+export PLT_LOCATION=_build/$MIX_ENV/$PLT_TESTNAME
+
+mix dialyzer --halt-exit-status
