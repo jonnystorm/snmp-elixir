e59927a39af63d6aad5eb9be1a4180006be3f895
Jonathan D. Storm <jds@idio.link>
2018-08-14 10:14:10 -0500

    Update dev_test.sh

--- dev_test.sh
+++ dev_test.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-while true; do
+function run
+{
+  mix clean &&
+    ERL_COMPILER_OPTIONS=bin_opt_info mix compile --force &&
+    mix test &&
+    env MIX_ENV=test mix dialyzer --halt-exit-status
+}
+
+clear
+run
+
+while true
+do
   inotifywait --exclude \..*\.sw. -re modify .
   clear
-  mix test &&
-    env MIX_ENV=test mix dialyzer --halt-exit-status
+  run
 done
