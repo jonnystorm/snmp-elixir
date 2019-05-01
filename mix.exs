b4f594b36295f6c0f061778e3185944c6e0bfe92
Tom Hoenderdos <tom.hoenderdos@tele2.com>
2019-05-01 10:48:40 +0200

    Fix version to be semver

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SNMP.Mixfile do
 
   def project do
     [ app: :snmp_ex,
-      version: "0.2",
+      version: "0.2.0",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
