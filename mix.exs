bdc068f81eaddd89ef354d2948c7fc65b570eab5
Tom Hoenderdos <tom.hoenderdos@tele2.com>
2019-05-01 10:48:10 +0200

    Make SNMP ready for Hex

--- mix.exs
+++ mix.exs
@@ -3,11 +3,13 @@ defmodule SNMP.Mixfile do
 
   def project do
     [ app: :snmp_ex,
-      version: "0.1.3",
+      version: "0.2",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
       dialyzer: [
         plt_add_apps: [
           :logger,
@@ -49,4 +51,18 @@ defmodule SNMP.Mixfile do
     [ {:netaddr_ex, "~> 1.0.5"},
     ]
   end
+
+  defp description do
+    "An SNMP client library for Elixir, wrapping the Erlang OTP SNMP API and Logic"
+  end
+
+    defp package do
+    [ licenses: ["Mozilla Public License 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/jonnystorm/snmp-elixir",
+      },
+    ]
+  end
+
+
 end
