507cbf91e5376b96f8dc76a3be253c788be27887
Jonathan Storm <jonnystorm@users.noreply.github.com>
2019-11-24 13:48:09 -0600

    Make varbind result a map instead of tuple (#45)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SNMP.Mixfile do
 
   def project do
     [ app: :snmp_ex,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
